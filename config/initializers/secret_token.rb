05cd49a9f32e226c0f8471ef34f5234b4fe9e0bd
Ryo Kato <katoryo55@gmail.com>
2012-09-02 22:47:17 +0900

    add navbar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prunion::Application.config.secret_token = 'd48efbc2a5134b33b3787143f9b7244dc9e29d23e1a7d97d8d13d87eceb6c5dfb3ebbf18d66dd98c6286c595b065402d8be61920d40dda244a89cec913365feb'
