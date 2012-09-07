a89418faf22a6419109e61ec1cf07062590fb3b1
Ryo Kato <katoryo55@gmail.com>
2012-09-07 20:26:57 +0900

    fix retweet, so it associate user and item

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -1,10 +1,12 @@
+#coding: utf-8
 class RetweetsController < ApplicationController
   def create
-    @retweet = current_user.retweets.new(params[:item])
+    @retweet = current_user.retweets.new(params[:id])
+    @retweet.item_id = params[:item_id]
     respond_to do |format|
       if @retweet.save
-        format.html { redirect_to @retweet, notice: 'Item was successfully created.' }
-        format.json { render json: @retweet, status: :created, location: @retweet }
+        format.html { redirect_to "/", notice: "宣伝に成功しました！" }
+        format.json { render json: "/", status: :created, location: @retweet }
       else
         format.html { render action: "new" }
         format.json { render json: @retweet.errors, status: :unprocessable_entity }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @items = Item.order("created_at DESC").limit(10)
+    @items = Item.order("created_at DESC")
+    @retweets = Retweet.all
   end
 end
