57c7b01f9fe1081fda11b4608d603157cebc58ad
Ryo Kato <katoryo55@gmail.com>
2012-09-23 22:07:49 +0900

    sessionをTwitter用にちょっと変えてみた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,18 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
   session[:user_id] = user.id
+  set_twitter_config(request.env["omniauth.auth"])
   redirect_to root_url, notice: "ログインしました"
   end
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "ログアウトしました"
+  end
+
+  def set_twitter_config(auth)
+    Twitter.configure do |config|
+      config.oauth_token = auth['credentials']['token']
+      config.oauth_token_secret = auth['credentials']['secret']
+    end
   end
 end
